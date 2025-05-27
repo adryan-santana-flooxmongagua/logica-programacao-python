95d83c8c68c0fc478182364e4e40d42a0e2cfba5
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-27 10:01:43 -0300

    feat: introduce code blocks + if / elif / else (conditionals)

---
+++ aula1/aula16.py
@@ -0,0 +1,16 @@
+# if / elif / else
+# se / senão se / senão
+
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+    print(12341234)
+
+elif entrada == 'sair':
+    print('Você saiu do sistema.')
+
+else:
+    print('Você não digitou nem "entrar" nem "sair".')
+
+print('FORA DOS BLOCOS')
