bc4021f58eef0430881a321d23f7051b3468b341
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-27 08:38:42 -0300

    feat: introduce str type and data types overview

---
+++ aula1/aula3.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('KKau man')
+print(1, 'Lcas "')
+
+# Aspas duplas
+print("edkalllos")
+print(2, "wowoa joao'")
+
+# Escape
+print("adryan \"alex\"")
+
+# r
+print(r"luan \"sant.\"")
