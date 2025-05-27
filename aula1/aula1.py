24596025a2319046d507ae61ef048de3dd9a8e2a
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-27 08:31:39 -0300

    feat: implement print function

--- aula1/aula1.py
+++ aula1/aula1.py
@@ -1,1 +1,15 @@
-print('hello word2')
+"""
+DocString
+E escrever o que eu
+quiser
+
+
+asdfasdfd
+"""
+
+''' Usar para escrever suas notas '''
+
+# Permite escrever um comentário
+print(123)  # Na frente
+# Abaixo
+print(456)
