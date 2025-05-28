6f479d392d590261413f17df3f4521b98824b861
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-05-28 09:51:21 -0300

    feat: show how to use assignment operators with arithmetic operators

---
+++ aula1/aula36.py
@@ -0,0 +1,18 @@
+"""
+Operadores de atribuição:
+=  (atribuição simples)
++= (adição e atribuição)
+-= (subtração e atribuição)
+*= (multiplicação e atribuição)
+/= (divisão e atribuição)
+//= (divisão inteira e atribuição)
+**= (exponenciação e atribuição)
+%= (módulo e atribuição)
+"""
+
+contador = 10
+
+# Neste caso, utilizamos o operador /= para dividir o valor atual de 'contador' por 5
+contador /= 5  # contador = contador / 5
+
+print(contador)  # Saída: 2.0
